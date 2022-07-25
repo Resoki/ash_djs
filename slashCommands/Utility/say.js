b4705ab88e8e6b73c8f379dccf71e31e66126808
Resoki <remyaubert71@gmail.com>
2022-07-25 19:07:28 +0200

    edit

---
+++ slashCommands/Utility/say.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  name: "say",
+  aliases: ["bdg"],
+  category: "Utility",
+  description: "Faire parler le bot",
+  usage: `**/say <word>**`,
+  ownerOnly: false,
+  options: [
+    {
+        name: "word",
+        description: "Le mot que le bot doit répeter",
+        type: 3,
+        required: false
+    }
+],
+  run: async (client, interaction, args) => {
+    try {
+     const word = interaction.options.getString('word');
+    return interaction.channel.send(word);
+    }
+    catch(err){
+      return interaction.channel.send(`❌ | Une erreur a eu lieu **say.js**:\n${err}`);
+    }
+  },
+};
