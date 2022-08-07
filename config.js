3199bd419c9263f90d2f93adffe61bce4022d8a3
Resoki <remyaubert71@gmail.com>
2022-08-07 16:10:42 +0200

    ediot

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  botClientID: "847377325136805908", // client application id du bot (trouveable sur https://discord.com/developers/application)
+  botClientID: "1001114437849206796", // client application id du bot (trouveable sur https://discord.com/developers/application)
   botPrefix: "!",
-  token: "ODQ3Mzc3MzI1MTM2ODA1OTA4.Gonc4q.StDl_V_7YWdrxCiyC5BdL75teOtpvZpoNLctho",
+  token: "MTAwMTExMzM4MDAzNzM0MTE4NA.GGrEn9.G1LuOkhD0dlEhkN_2aTfuaHsBBFIx7K9SUZYUg",
   channelJoin:"960931567927164979",
-  guildID: "965542937343692820", // id server
+  guildID: "1001113010431078490", // id server
   ownerID: "923924088077053972", // id du proprietaire du serveur
   embedColor: "#00dbff",
   embedfooterText: "Ash 2022",
-  ticketsSupportRoles: ["960927743367839794"],
-  ticketsTranscripts: '978655935503417435',
-   
-   channelMessages: "965591382163062894"
+  channelMessages: "1001113010431078493"
 }
