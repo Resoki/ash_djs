2e81ac33d49d711353df748e8ed80baed166844b
Remy.Aubert <r.aubert@ldlc.com>
2022-08-09 10:29:28 +0200

    edit memeberjoin

--- slashCommands/Utility/resetpointsall.js
+++ slashCommands/Utility/resetpointsall.js
@@ -10,12 +10,12 @@ module.exports = {
   run: async (client, interaction, args) => {
     try {
       const permission = interaction.member.permissions.has(client.discord.Permissions.FLAGS.BAN_MEMBERS);
-      if (!permission) return interaction.reply({content:`❌ | Tu n'as pas la permission d'utiliser cette commande !`, ephemeral: true});
+      if (permission) return interaction.reply({content:`❌ | Tu n'as pas la permission d'utiliser cette commande !`, ephemeral: true});
 
         let money = db.all()
 
         if(!money) return;
-        
+
          money.map(entry => entry.ID)
         .filter(id => id.startsWith(`pointsTab_`))
          money.forEach(db.delete)
